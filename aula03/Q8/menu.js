fbf2e47a5b53e007a3b45ebe9eeefdd64e1ebfb1
IsequielNascimento <isequielnascimento@gmail.com>
2023-12-21 08:24:04 -0300

    Conclusão Q8 Aula03

---
+++ aula03/Q8/menu.js
@@ -0,0 +1,8 @@
+/*Refaça a questão 04 utilizando Common JS. Você deve criar um arquivo "menu.js" e outro arquivo "operacoes.js" para essa questão.*/
+import { operacao } from './operacoes.js'
+
+console.log(" Operações válidas: soma, subtração, multiplicação, divisão.")
+let opera = "multiplicação"
+
+operacao(opera,4,2)
+
