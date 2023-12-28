361c4304fca12f4dccd7fce5700f454f2ba9a4ca
IsequielNascimento <isequielnascimento@gmail.com>
2023-12-28 10:21:23 -0300

    questão 1 aula 06

---
+++ aula06/Q1.js
@@ -0,0 +1,10 @@
+/*1. Ceverino tem uma estante com três compartimentos um em cima do outro. Cada compartimento comporta três caixas uma ao lado da outra. As caixas possuem duas características que o Ceverino sempre observa: cor da caixa e a numeração escrita nela. Sabendo disso, escolha uma estrutura de dados para representar a estante de Ceverino e um tipo de dado para representar a caixa.
+*/
+
+const estante = [
+    [{id: 1, cor: "vermelho"},{id: 2, cor: "azul"},{id: 3, cor: "amarelo"}],
+    [{id: 4, cor: "cor sim"},{id: 5, cor: "cor não"},{id: 6, cor: "roxo"} ],
+    [ {id: 7, cor: "vermelho"},{id: 8, cor: "vermelho 2"},{id: 9, cor: "vermelho 3"}    ],
+]
+
+console.log(estante[1][1])
